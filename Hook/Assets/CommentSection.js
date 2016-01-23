76e0cc00e702999a7c7cf7c94d590ffb41e856eb
Luke Jones <luke@freerunningtechnologies.com>
2016-01-23 16:54:10 -0500

    Add switching functionality for comment selector
    This commit formats the coffee script into nice modular functions as
    well as adding functionality to switch between comment sections by
    clicking the comment selector.

--- Hook/Assets/CommentSection.js
+++ Hook/Assets/CommentSection.js
@@ -2,7 +2,28 @@
 (function() {
 
   $(document).ready(function() {
-    return $('<div class="branded-page-box yt-card" id="comments-selector"><div id="reddit-select" class="selected">Reddit</div><div id="youtube-select">Youtube</div></div>').insertBefore('#watch-discussion');
+    var pageSetup, selectorSetup;
+    pageSetup = function() {
+      $('<div class="branded-page-box yt-card" id="comments-selector"><div id="reddit-select" class="selected">Reddit</div><div id="youtube-select">Youtube</div></div>').insertBefore('#watch-discussion');
+      $('#watch-discussion').hide();
+      return $('<div class="branded-page-box yt-card" id="reddit-comments-card">CONTENT</div>').insertAfter('#watch-discussion');
+    };
+    selectorSetup = function() {
+      $('#reddit-select').on("click", function() {
+        $('#watch-discussion').hide();
+        $('#reddit-comments-card').show();
+        $('#reddit-select').addClass('selected');
+        return $('#youtube-select').removeClass('selected');
+      });
+      return $('#youtube-select').on("click", function() {
+        $('#watch-discussion').show();
+        $('#reddit-comments-card').hide();
+        $('#reddit-select').removeClass('selected');
+        return $('#youtube-select').addClass('selected');
+      });
+    };
+    pageSetup();
+    return selectorSetup();
   });
 
 }).call(this);
--- dev/CommentSection.coffee
+++ dev/CommentSection.coffee
@@ -1,2 +1,22 @@
 $(document).ready ->
-  $('<div class="branded-page-box yt-card" id="comments-selector"><div id="reddit-select" class="selected">Reddit</div><div id="youtube-select">Youtube</div></div>').insertBefore('#watch-discussion')
+  pageSetup = ->
+    $('<div class="branded-page-box yt-card" id="comments-selector"><div id="reddit-select" class="selected">Reddit</div><div id="youtube-select">Youtube</div></div>').insertBefore('#watch-discussion')
+    $('#watch-discussion').hide()
+    $('<div class="branded-page-box yt-card" id="reddit-comments-card">CONTENT</div>').insertAfter('#watch-discussion')
+
+  selectorSetup = ->
+    $('#reddit-select').on "click", ->
+      $('#watch-discussion').hide()
+      $('#reddit-comments-card').show()
+      $('#reddit-select').addClass('selected')
+      $('#youtube-select').removeClass('selected')
+
+    $('#youtube-select').on "click", ->
+      $('#watch-discussion').show()
+      $('#reddit-comments-card').hide()
+      $('#reddit-select').removeClass('selected')
+      $('#youtube-select').addClass('selected')
+
+  #Main Script
+  pageSetup()
+  selectorSetup()
