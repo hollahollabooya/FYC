926f248fbad28130a68b044d0a6957067704cb3b
hollahollabooya <michaelecoppinger@gmail.com>
2016-01-23 18:10:53 -0500

    Pulls all relevant data from RedditDB.

--- Hook/assets/RedditSearch.js
+++ Hook/assets/RedditSearch.js
@@ -1,32 +1,34 @@
-var RedditSearch = new Object();
+var RedditSearch = function(callback){
+    this.callback = callback;
+};
 
-RedditSearch.prototype.generateQuery = function(url) {
+RedditSearch.prototype.queryReddit = function(url) {
+    $.ajax({
+        url:"http://www.reddit.com/search.json",
+        data: {limit: 5, q:("url:" + url), sort: "top"},
+        success: this.getCommentTrees.bind(this)});
+};
+
+RedditSearch.prototype.queryCommentTree = function(subreddit, id) {
     return $.ajax({
-        url: "http://www.reddit.com/search.json",
-        method: "GET",
-        data: {
-            after:"",
-            before:"",
-            count: 5,
-            include_facets: false,
-            limit: 5,
-            q:"url:https://www.youtube.com/watch?v=0NvI1BMCfe0",
-            restrict_sr: false,
-            sort: "top",
-            syntax: "plain",
-            t:"all"
-        }
-    });
+        url:"http://www.reddit.com/r/" + subreddit + "/" + id + ".json",
+        data: {depth: 2, sort: "top"}});
 };
 
-RedditSearch.prototype.getComments = function(url) {
-    // Fetch the results of a query for the URL
-    var results = RedditSearch.generateQuery(url);
-    return results;
+RedditSearch.prototype.getCommentTrees = function(data) {
+    var results = data;
+    var commentTrees = [];
+    var length = results.data.children.length;
+    for (var i = 0; i < length; i++) {
+        commentTrees[i] = this.queryCommentTree(
+            results.data.children[i].data.subreddit,
+            results.data.children[i].data.id);
+    }
+    $.when.apply($, commentTrees).then(function(){
+        var args = arguments;
+        console.log(args)
+    })
 };
 
-RedditSearch.prototype.getCommentTree = function() {
-
-};
-
-var test = RedditSearch.getComments("https://www.youtube.com/watch?v=0NvI1BMCfe0");
+var reddit = new RedditSearch();
+var test = reddit.queryReddit("https://www.youtube.com/watch?v=yZClGkWUP8I");
