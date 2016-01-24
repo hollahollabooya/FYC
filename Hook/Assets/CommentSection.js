97f45ce7e9e07eaccca56b04dfc076bff32360fc
Luke Jones <luke@freerunningtechnologies.com>
2016-01-24 07:05:59 -0500

    Add subreddit styling and display
    Adds subreddit stylings and display.

--- Hook/Assets/CommentSection.js
+++ Hook/Assets/CommentSection.js
@@ -42,16 +42,17 @@
         $('#reddit-comments-card').append(commentHTML);
         commentHTML.append('<div class="reddit-comment-updoot"></div>');
         commentHTML.find('.reddit-comment-updoot').append("<div class=\"up\" style=\"background-image: url(" + (chrome.extension.getURL('/Assets/Images/reddit_icons.png')) + ");\"></div>");
-        commentHTML.find('.reddit-comment-updoot').append("<span>" + comment.upVotes + "</span>");
+        commentHTML.find('.reddit-comment-updoot').append("<span>" + comment.score + "</span>");
         commentHTML.find('.reddit-comment-updoot').append("<div class=\"down\" style=\"background-image: url(" + (chrome.extension.getURL('/Assets/Images/reddit_icons.png')) + ");\"></div>");
         commentHTML.append('<div class="comment-wrapper"></div>');
         commentHTML.find('.comment-wrapper').append('<div class="reddit-comment-user"></div>');
         commentHTML.find('.reddit-comment-user').append("<a href=\"http://www.reddit.com/u/" + comment.user + "\">" + comment.user + "</a>");
+        commentHTML.find('.comment-wrapper').append("<div class=\"reddit-comment-sub\">r/" + comment.subreddit + "</div>");
         commentHTML.find('.comment-wrapper').append("<div class=\"reddit-comment-content\">" + (decodeHTML(comment.userContent)) + "</div>");
         commentHTML.find('.comment-wrapper').append('<div class="reddit-comment-source"></div>');
         commentHTML.find('.reddit-comment-source').append("<a href=\"" + comment.url + "\">See Original</a>");
         commentHTML.find('.comment-wrapper').append('<div class="reddit-comment-more"></div>');
-        commentHTML.find('.reddit-comment-more').append("<a href=\"" + comment.thread + "\">More From This Thread</a>");
+        commentHTML.find('.reddit-comment-more').append("<a href=\"" + comment.threadUrl + "\">More From This Thread</a>");
         if (index === 0) {
           _results.push(populateComments(comment.children, 1));
         } else {
