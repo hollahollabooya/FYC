b5b2f1fed1161bcf8aa510de137ec2c4d6a6f579
Luke Jones <luke@freerunningtechnologies.com>
2016-01-26 16:04:35 -0500

    Add delayed injection loading
    This commit causes the data to load on the first sign of the page
    changing, but waits until the page is loaded enough before injecting.
    This means injection is fast, data loading is undetectable, and our
    comments will display in synch with youtube comments. Code is pretty
    hack jobby, so a basic rundown is. Load data and ready injection are
    called to mark when injection is possible and when data has been loaded.
    They then both call a handler which will only run the main script if
    both the data is loaded and the page is ready for injection. There are
    some places where the variables used to track this are reset to false.
    They may appear abitrary but they have a purpose i promise ;)

--- Hook/reddit_query.js
+++ Hook/reddit_query.js
@@ -1,20 +1,54 @@
-function afterNavigate() {
+var comments = [];
+var dataReady = false;
+var injectionReady = false;
+
+function injectionRuntimeHandler() {
+  console.log(dataReady);
+  console.log(injectionReady);
+  console.log('');
+  if (dataReady && injectionReady) {
+    injectionScript(comments);
+    dataReady = false;
+    injectionReady = false;
+  }
+}
+
+function loadData() {
     if ('/watch' === location.pathname) {
-        var url = window.location.href;
-        mainScript();
+      reddit = new RedditSearch(function (foundComments) {
+        dataReady = true;
+        comments = foundComments;
+        injectionRuntimeHandler();
+      });
+      dataReady = false;
+      injectionReady = false;
+      reddit.queryReddit(window.location.href)
     }
 }
+
+function readyInjection() {
+  injectionReady = true;
+  console.log($('#watch-discussion'));
+  injectionRuntimeHandler();
+}
+
 var observer = new WebKitMutationObserver(function(mutations) {
   mutations.forEach(function(mutation) {
     for (var i = 0; i < mutation.addedNodes.length; i++) {
       var node = mutation.addedNodes[i];
       if (node.tagName === "DIV" && node.id === "watch7-container") {
-        afterNavigate();
+        loadData();
+      }
+      if (node.tagName === "DIV" && node.id === "yt-comments-sb-container") {
+        readyInjection();
       }
     }
    })
 });
+
 observer.observe(document, { childList: true, attributes: true, subtree: true, characterData: true });
+
 $(document).ready(function() {
-  afterNavigate();
+  loadData();
+  readyInjection();
 });
